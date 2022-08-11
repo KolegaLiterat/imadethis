455e7e715b5b91faafe39b53f5ece0857792b3cc
Adrian "Literat" Jaworek <adrian@kolegaliterat.pl>
2022-08-11 18:31:55 +0200

    Add temp and hum sensor

--- arduinoProjects/plantsHelper/plantsHelper.ino
+++ arduinoProjects/plantsHelper/plantsHelper.ino
@@ -1,21 +1,33 @@
+#include <DHT.h>
+
+//JOYSTICK CONFIG START
 /**
 Unused config for axis changes in joystick
 #define JOYSTICK_X A0
 #define JOYSTICK_Y A1
 **/
 #define JOYSTICK_SWITCH 2
+//JOYSTICK CONFIG END
+
+//SENSOR CONFIG START
+DHT dht(3, DHT22);
+//SENSOR CONFIT END
 
 void setup() {
   Serial.begin(9600);
   pinMode(JOYSTICK_SWITCH, INPUT_PULLUP);
-
+  dht.begin();
 }
 
 void loop() {
-  int joystick_state = get_state();
+  int joystick_state = get_state();;
 
   if (joystick_state == 0) {
-    Serial.println("Change!");
+    Serial.println("Humidity: ");
+    Serial.println(dht.readHumidity());
+
+    Serial.println("Temperature: ");
+    Serial.println(dht.readTemperature());
   }
 }
 
