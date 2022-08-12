c53f46fc1397b927451625d5dfd50baf18a502a8
Adrian "Literat" Jaworek <adrian@kolegaliterat.pl>
2022-08-12 10:27:24 +0200

    8 x 8 matrix display
    Added cloud print
    Added printing method

--- arduinoProjects/plantsHelper/plantsHelper.ino
+++ arduinoProjects/plantsHelper/plantsHelper.ino
@@ -1,4 +1,5 @@
 #include <DHT.h>
+#include <LedControl.h>
 
 //JOYSTICK CONFIG START
 /**
@@ -13,14 +14,35 @@ Unused config for axis changes in joystick
 DHT dht(3, DHT11);
 //SENSOR CONFIT END
 
+//LED MATRIX CONFIG START
+LedControl led_matrix = LedControl(12,10,11,0);
+
+byte cloud[8] = {
+  B00011100,
+  B00111100,
+  B01111110,
+  B11111111,
+  B11111111,
+  B00000000,
+  B01001001,
+  B10010010,
+  };
+//LED MATRIX CONFIG STOP
+
 void setup() {
   Serial.begin(9600);
+  
   pinMode(JOYSTICK_SWITCH, INPUT_PULLUP);
+  
   dht.begin();
+
+  led_matrix.shutdown(0,false);
+  led_matrix.setIntensity(0,15);
+  led_matrix.clearDisplay(0);
 }
 
 void loop() {
-  int joystick_state = get_state();;
+  int joystick_state = get_state();
 
   if (joystick_state == 0) {
     Serial.println("Humidity: ");
@@ -28,6 +50,10 @@ void loop() {
 
     Serial.println("Temperature: ");
     Serial.println(dht.readTemperature());
+
+    print_picture(cloud);
+    delay(1000);
+    led_matrix.clearDisplay(0);
   }
 }
 
@@ -36,4 +62,12 @@ int get_state()
   int switch_state = digitalRead(JOYSTICK_SWITCH);
 
   return switch_state;
+}
+
+void print_picture(byte character [])
+{ 
+  for(int i = 0; i < 8; i++)
+  {
+    led_matrix.setRow(0, i, character[i]);
+  }
 }
